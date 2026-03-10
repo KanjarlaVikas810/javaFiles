d8c57332642399864ee398e302f45ee9b320bf98
KanjarlaVikas810 <vikaskanjarla810@gmail.com>
2026-03-10 14:23:10 +0530

    Update ON3.java

--- ON3.java
+++ ON3.java
@@ -2,15 +2,8 @@ public class ON3Example {
     public static void main(String[] args) {
         int n = 5;
 
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {           // O(n)
-            for (int j = 0; j < n; j++) {       // O(n)
-                for (int k = 0; k < n; k++) {   // O(n)
-                    count++;
-                }
-            }
-        }
+        // Use long to avoid overflow when n^3 exceeds Integer.MAX_VALUE
+        long count = 1L * n * n * n;
 
         System.out.println("Total operations: " + count);
     }
