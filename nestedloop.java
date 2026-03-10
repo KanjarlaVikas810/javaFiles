f398a7723b7f4bfaeba04b4b8f358067793f551f
KanjarlaVikas810 <vikaskanjarla810@gmail.com>
2026-03-10 15:16:29 +0530

    Update nestedloop.java

--- nestedloop.java
+++ nestedloop.java
@@ -1,10 +1,11 @@
-public class NestedLoopExample {
+public class NestedLoop {
     public static void main(String[] args) {
 
-        String row = "1 2 3 4";
-        for (int i = 0; i < 3; i++) {
-            System.out.println(row);
-        }
+        System.out.print(
+            "1 2 3 4\n" +
+            "1 2 3 4\n" +
+            "1 2 3 4\n"
+        );
 
     }
 }
