ec874cb07c5cb2297b819f539e190460f815095a
KanjarlaVikas810 <vikaskanjarla810@gmail.com>
2026-03-17 11:21:17 +0530

    Update ON3.java

--- ON3.java
+++ ON3.java
@@ -4,6 +4,16 @@ public class ON3Example {
 
         // Use long to avoid overflow when n^3 exceeds Integer.MAX_VALUE
         long count = 1L * n * n * n;
+        for (int i = 1; i <= 3; i++) {            // outer loop
+            System.out.println();        
+            for (int j = 1; i <= 3; i++) {            // outer loop
+            System.out.println();  
+                for (int k = 1; i <= 3; i++) {            // outer loop
+            System.out.println();                 // new line after each row
+        }// new line after each row
+            }    // new line after each row
+            
+        }
 
         System.out.println("Total operations: " + count);
     }
