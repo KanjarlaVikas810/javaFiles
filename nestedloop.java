2c0bb246e88892c05fc5f21e8bbbc58c0310d581
KanjarlaVikas810 <vikaskanjarla810@gmail.com>
2026-03-17 11:44:03 +0530

    Update nestedloop.java

--- nestedloop.java
+++ nestedloop.java
@@ -6,10 +6,7 @@ public class NestedLoop {
             "1 2 3 4\n" +
             "1 2 3 4\n"
         );
-        for (int i = 1; i <= 3; i++) {            // outer loop
-            System.out.println();        
-          
-        }
+      
 
     }
 }
